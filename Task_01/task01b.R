e94a0ff78813b4a2301f88c4e25daf20496b3b3b
lto0001 <lto0001@mix.wvu.edu>
2020-01-16 14:47:20 -0500

    Lauren Oxford Teask 01 upload b

--- Task_01/task01b.R
+++ Task_01/task01b.R
@@ -1,28 +1,33 @@
-install.packages("swirl", dep=T)
-library(swirl)#
-#
-swirl()
 install.packages("swirl")
+
 library(swirl)
+
 swirl()
+1
+1
+1
+1
+
 5 + 7
 x <- 5 + 7
 x
 y <- x - 3
 y
-c(1.1, 9, 3.14)
 z <- c(1.1, 9, 3.14)
+
 ?c
 z
 c(z, 555, z)
 z * 2 + 100
-my_sqrt <- z - 1
-sqrt(z - 1)
 my_sqrt <- sqrt(z - 1)
+1
 my_sqrt
 my_div <- z / my_sqrt
+
 my_div
 c(1, 2, 3, 4) + c(0, 10)
 c(1, 2, 3, 4) + c(0, 10, 100)
 z * 2 + 1000
 my_div
+yes
+0
