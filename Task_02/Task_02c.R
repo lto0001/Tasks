1b3b3098d3a19267a3abaf34602b02d6ee43e909
lto0001 <lto0001@mix.wvu.edu>
2020-01-26 17:42:47 -0500

    Lauren Oxford Task 02 upload c

--- Task_02/Task_02c.R
+++ Task_02/Task_02c.R
@@ -1,4 +1,4 @@
-# Hypothesis: Did learning the skill to crawl fully increase the rate Beren's milk 
+# Hypothesis: Did learning the skill to crawl fully increase the rate of Beren's milk per feeding in ounces
 
 # Read in data & set up working directory
 setwd("~/Desktop/Evolution/Tasks/Task_02")
@@ -6,6 +6,7 @@ beren3 <- read.csv("beren_new.csv", stringsAsFactors = F)
 
 # Find which row he learned to crawl
 Skill <- which(beren3$event == "skill_crawl_full")
+Skill <- grep("high", beren3$event)
 
 # Find day he learned to crawl
 Age <- beren3[Skill, "age"]
@@ -18,13 +19,24 @@ berenOlder <- beren3[which(beren3$age > Age),]
 Feeds <- which(berenYounger$event == "bottle")
 
 # Use mean() on value column to get overall avg milk for subset younger
-youngerOverallAvgMilk <- mean(berenYounger$value["Feeds"])
+youngerOverallAvgMilk <- mean(berenYounger$value[Feeds])
 
 # Use tapply() on value & age to get per-day avg milk for younger
-youngerPerDayAvgMilk <- 
+youngerPerDayAvgMilk <- tapply(berenYounger$value[Feeds], berenYounger$age[Feeds], mean)
 
 # Use mean() on youngerPerDayAvgMilk to get avg milk before crawl
-beforeAvgMilk <-
+beforeAvgMilk <- mean(youngerPerDayAvgMilk)
 
 ### Repeat above 4 steps for older
-olderOverallAvgMilk <- 
+Feeds <- which(berenOlder$event == "bottle")
+olderOverallAvgMilk <- mean(berenOlder$value[Feeds])
+olderPerDayAvgMilk <- tapply(berenOlder$value[Feeds], berenOlder$age[Feeds], mean)
+
+#Now make a pot but dont use cammand z and compare older and younger (two plots)
+
+boxplot(youngerPerDayAvgMilk, olderPerDayAvgMilk)
+par(mfrow=c(1,2))
+plot(as.numeric(names(youngerPerDayAvgMilk)), youngerPerDayAvgMilk, type="b" , xlab="Age in Days" , ylab="Ounces of Milk" , main="Before Learning to Crawl Full")
+plot(as.numeric(names(olderPerDayAvgMilk)), olderPerDayAvgMilk, type="b" , xlab="Age in days" , ylab="Ounces of milk" , main="After Learning to Crawl Full ")
+
+#To label x axis use "xlab", to label y axis use "ylab", and to title a graph use "main" 
